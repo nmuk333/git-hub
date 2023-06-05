d85af579a5e3a620c0db8870749c6dbe3d527ffb
islahanleybrown <133700680+islahanleybrown@users.noreply.github.com>
2023-06-05 21:03:00 +0000

    changes to project

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import requests
-
+import pp
 def search_shows_by_actor(actor):
   url = f'https://api.tvmaze.com/search/people?q={actor}'
   response = requests.get(url)
   response.raise_for_status()
   data = response.json()
+
+
   
   for result in data:
+    print (result["score"])
     person = result["person"]
     print (person["deathday"])
     print (person["birthday"])
@@ -15,9 +18,11 @@ def search_shows_by_actor(actor):
   
   return data
 
+
 # Example usage
 actor = input('Enter actor name: ')
 shows = search_shows_by_actor(actor)
+
 
 if shows:
    print(f'Information about {actor}:')
