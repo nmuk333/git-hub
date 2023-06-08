d4fe143cae08d83396eeb6f6b27981251d763cc7
nmuk333 <133701099+nmuk333@users.noreply.github.com>
2023-06-08 21:18:14 +0000

    debug/viewing

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ def search_shows_by_actor(actor):
   
   for result in data:
     person = result["person"]
-    if person["birthday"] is not None:
-      print ("Birthday: " + person["birthday"])
-    else:
-      print("Birthday is unknown")
     if person ["deathday"] is not None:
       print ("Deathday: " + person['deathday'])
     else:
       print("Still alive")
+    if person["birthday"] is not None:
+      print ("Birthday: " + person["birthday"])
+    else:
+      print("Birthday is unknown")
     if person ["gender"] is not None:
       print ("Gender: " + person["gender"])
     else:
       print ("Gender not known")
 
   return data
-actor = input('Enter actor name: ')
+actor = input('Please enter the name of an actor/actress: ')
 shows = search_shows_by_actor(actor)
 
 
